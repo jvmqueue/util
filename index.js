9b49a288f25c45c194f1f7477bff3dbfbced03dd
jvmqueue <wheels0090@yahoo.com>
2015-06-13 08:37:14 -0400

    adding meaningful comments, code clean up

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 (function(w, d, $, undefined){
 
+/*
+    index.js only used to test httpRequest.js
+    We kept index.js as simple as possible. With hope, allowing others to employ our scripts with ease
+    Note: index.js is started by intervalDom at the bottom of this file
+    Note: once intervalDom completes its work, control is handed to main()
+*/
+
     var main = function(){
 
         jvm.dom.setListener({ // define custom event for httpRequest to trigger once data retrieved
@@ -10,12 +17,14 @@
         });
         // the $node and event to trigger once data retrieved
         // httpRequest sends data back through triggering handler
+        // Note: $node can be any existing node
+        // Note: event can be any event, custom or native
         var req = jvm.httpRequest.fnc.getData({ // could wait for !!req and get data via req.requestText
-            path:'data/education.json',
-            cache:true,
-            fileType:'json',
+            $node:$('#container'),
             event:'data:retrieved',
-            $node:$('#container')
+            path:'data/education.json',
+            fileType:'json',
+            cache:true
         });
     };
 
@@ -39,7 +48,7 @@
     var intervalDom = w.setInterval(function(){ // wait for DOM, we do not need jQuery for this
         if(d.getElementsByTagName('div').length > 0){             
             w.clearInterval(intervalDom);
-            main();
+            main(); // start main
         }
     }, 33);
 })(window, document, jQuery);
