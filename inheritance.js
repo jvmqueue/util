62e3461ebc312a0ddd649d059fe34e39900bf015
jvmqueue <wheels0090@yahoo.com>
2016-06-16 10:43:27 -0400

    define extendClassic for classical inheritance

--- inheritance.js
+++ inheritance.js
@@ -7,6 +7,20 @@ jvm.extend = (function(){
                     subClass.prototype[name] = superClass.prototype[name];
                 }
             }
+        },
+        extendClassic:function(subClass, superClass){
+            /*
+            * first statement in the SubClass: SubClass.superClass.constructor.call(this, <any parameters your passing to superClass>);
+            */            
+            var F = function(){};
+            F.prototype = superClass.prototype;
+            subClass.prototype = new F(); // prevents having to instantiate superClass
+            subClass.prototype.constructor = subClass; // destroyed constructor when we over wrote its prototype
+
+            subClass.superClass = superClass.prototype;
+            if(superClass.prototype.constructor == Object.prototype.constructor){ // do not want subclass to call Object's constructor
+                superClass.prototype.constructor = superClass; // ensures subClass calls the correct constructor
+            }            
         }
     };
     return{fnc:_fnc};
